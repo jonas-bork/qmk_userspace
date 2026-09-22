6bf8464f54d420c1c6225adda43ac88a53a14bfe
Jonas Bork <jonas10bork@gmail.com>
2026-09-22 18:28:28 +0200

    make holding a custom key work

--- keyboards/splitkb/halcyon/elora/keymaps/my_hlc/keymap.c
+++ keyboards/splitkb/halcyon/elora/keymaps/my_hlc/keymap.c
@@ -80,7 +80,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [_QWERTY] = LAYOUT(
      KC_ESC  , KC_1 ,  KC_2   ,  KC_3  ,   KC_4 ,   KC_5 ,                                        KC_6 ,  KC_7 ,  KC_8 ,   KC_9 ,  KC_0 , KC_BSPC,
-     KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , MKC_AA , // The last should be å
+     KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , MKC_AA ,
      CTL_ESC , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,MKC_AE, MKC_OE,
      KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,KC_CAPS,     FKEYS  , KC_RBRC, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
                                 ADJUST , KC_LGUI, ALT_ENT, KC_SPC , NAV   ,     SYM    , KC_SPC ,KC_RALT, KC_RGUI, KC_APP
@@ -254,17 +254,20 @@ bool key_with_shift(keyrecord_t* record, uint16_t key, bool key_is_shifted, uint
             } else {
                 uint8_t current_mods = get_mods();
                 del_mods(MOD_MASK_SHIFT);
-                tap_key(shift_key);
+                register_code16(shift_key);
                 set_mods(current_mods);
             }
         } else {
             if (key_is_shifted) {
-                tap_key(S(key));
+                register_code16(S(key));
             } else {
-                tap_key(key);
+                register_code16(key);
             }
         }
+    } else {
+        unregister_code16(key);
     }
+
     return false;
 }
 
